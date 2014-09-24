1cb8701bae622992952edbbb627b7689c89eca30
Jahnavi Kalyani <jahnavi@digitalgreen.org>
2014-09-24 12:22:19 +0530

    Changes to header

--- headbar.php
+++ headbar.php
@@ -12,8 +12,7 @@ if(isset($_SESSION['email_ses']))
 <?php if($login==0) { ?>
 <a href="all_courses.php" style="margin-right:10px;"  >Courses</a>
 <a href="#" style="margin-right:10px;" onclick="create_modal('login_modal'); return false;">Login</a>
-
-<a href="login.php">Signup</a>
+<a href="#" onclick="create_modal('signup_modal'); return false;">SignUp</a>
 
 <?php }
 else { 
@@ -22,15 +21,58 @@ $result1=mysql_query($sql1);
 
 			$row=mysql_fetch_assoc($result1);
 			$name=$row['name'];
+			$role=$row['role']
 
 	?>
 <a href="all_courses.php" style="margin-right:10px;"  >Courses</a>
-<a href="dashboard.php" style="margin-right:10px;" ><?php echo $name; ?></a>
+<a href="<?php if($role=='teacher') echo "instructor_dashboard.php"; else echo "dashboard.php"; ?>" style="margin-right:10px;" ><?php echo $name; ?></a>
 <a href="logout.php" style="margin-right:10px;"  >Logout</a>
 
 <?php } ?>
 </span>
 
 <center><a href="index.php">Doorado</a></center>
+
+</div>
+
+<div class="overlay_modal" id="login_modal">
+  <div class="overlay_modal_body">
+ <center>Login to your Doorado Account</center>
+
+  <form action="login.php" method="post" style="position:absolute; left:15%; width:30% ; top:150px">
+    Student Login
+    <fieldset id="inputs">
+                <input id="username" name="email" type="text" class="form-control" placeholder="Email" autofocus required>   
+                <input id="password" name="password" type="password"  class="form-control" placeholder="Password" required>
+            </fieldset>
+                    
+ 
+            <fieldset id="actions">
+                <input  type="submit" id="submit"  class="btn btn-danger" value="Sign in">
+                      
+             
+                  </fieldset>
+               </form>
+                 
+  <form style="position:absolute; left:55%; width:30%; top:150px" action="login.php" method="post">
+    Instructor Login
+    <fieldset id="inputs">
+                <input id="username" name="email" type="text" class="form-control" placeholder="Email" autofocus required>   
+                <input id="password" name="password" type="password"  class="form-control" placeholder="Password" required>
+            </fieldset>
+                   
+ 
+            <fieldset id="actions">
+                <input  type="submit" id="submit" class="btn btn-danger" value="Sign in">
+                      
+             
+                  </fieldset>
+  </form>
+
+
+
+
+  </div>
+
 
 </div>
